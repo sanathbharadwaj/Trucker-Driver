e88e3463619d32ec3c7f9839f2f800636f74729c
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2017-11-23 23:04:49 +0530

    Initial revision

---
+++ app/src/main/java/com/harsha/truckerdriver/MainActivity.java
@@ -0,0 +1,13 @@
+package com.harsha.truckerdriver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
